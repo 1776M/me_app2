e49fc1e19f66871af51c0c491a0eb02fd1cb4007
root <root@plato.kherahq.com>
2012-03-17 17:58:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeApp2::Application.config.secret_token = '9e9f88ee7024341e312fb757e5166ee4509c09bf2b360c7387737dc8ba0ab6015d958fbf7773cb86b6ceaa8d86b1447209f583af8a31a4821744a17f5e45d791'
